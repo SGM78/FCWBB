b29b03b861ab234920961dde98bb4c5add7016a3
Marchenkov Sergei <serguei1978@mail.ru>
2024-05-19 21:48:03 +0300

    Создпние программы

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] inputArray = { "1234", "1567", "-2", "computer science" };
+        
+        int count = 0;
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] resultArray = new string[count];
+        int index = 0;
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                resultArray[index] = str;
+                index++;
+            }
+        }
+
+        Console.WriteLine("[" + string.Join(", ", resultArray) + "]");
+    }
+}
